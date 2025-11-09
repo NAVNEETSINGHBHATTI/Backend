e2a94fd0135b6c8db7fb7f0f0ef08060568ce74d
Navneet <navneet122002@gmail.com>
2025-11-10 00:58:13 +0530

    Add Healthcheck Controller

--- src/app.js
+++ src/app.js
@@ -23,6 +23,7 @@ import likeRouter from "./routes/like.routes.js"
 import commentRouter from "./routes/comment.routes.js"
 import tweetRouter from "./routes/tweet.routes.js"
 import dashboardRouter from "./routes/dashboard.routes.js"
+import healthcheckRouter from "./routes/healthcheck.routes.js"
 
 // routes declaration
 
@@ -34,6 +35,7 @@ app.use("/api/v1/likes",likeRouter)
 app.use("/api/v1/comments",commentRouter)
 app.use("/api/v1/tweets",tweetRouter)
 app.use("/api/v1/dashboards",dashboardRouter)
+app.use("/api/v1/checks",healthcheckRouter)
 
 
 export {app}
